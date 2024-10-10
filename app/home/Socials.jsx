2891189b36005151933051cffdb882a7f4e7d9ef
Rownok Zahan <rownokzahan9@gmail.com>
2024-10-10 11:31:52 +0600

    Fix image position

--- app/home/Socials.jsx
+++ app/home/Socials.jsx
@@ -1,7 +1,7 @@
 import bgImage from "@/public/images/socials-bg.jpg";
 import instaImage from "@/public/images/insta.jpg";
-import facebookImage from "@/public/images/insta.jpg"; // Ensure the correct image is used
-import tiktokImage from "@/public/images/insta.jpg"; // Ensure the correct image is used
+import facebookImage from "@/public/images/insta.jpg";
+import tiktokImage from "@/public/images/insta.jpg";
 import instaIcon from "@/public/icons/instagram.svg";
 import facebookIcon from "@/public/icons/facebook.svg";
 import tiktokIcon from "@/public/icons/tiktok.svg";
@@ -11,79 +11,84 @@ const Socials = () => {
   return (
     <>
       <section
-        className="h-screen bg-cover bg-center hidden md:block"
+        className="min-h-screen bg-cover bg-left hidden sm:block"
         style={{ backgroundImage: `url('${bgImage.src}')` }}
       >
-        <div className="container !mt-0 pt-12">
-          <h2 className="text-6xl font-bold">Find Us On Socials</h2>
-          <div className="mt-12 w-1/2 grid grid-cols-3 gap-4 ms-12">
-            <div className="col-span-2">
-              <div className="-space-y-8">
-                <div className="w-56 bg-ui-white shadow-lg relative">
+        <div className="container !mt-0 py-12">
+          <h2 className="mb-12 text-5xl font-bold">Find Us On Socials</h2>
+          <div className="flex items-center">
+            <div className="w-52 me-[208px]">
+              <div className="relative">
+                <figure className="w-full aspect-[5/6] rounded-3xl shadow-lg overflow-hidden">
                   <Image
                     width={224}
                     height={400}
                     src={instaImage.src}
                     alt="Instagram"
-                    className="rounded-xl"
+                    className="w-full h-full rounded-3xl object-cover hover:scale-110 duration-1000"
                   />
-                  <div className="absolute -top-4 -right-4">
-                    <Image
-                      className="w-10"
-                      width={96}
-                      height={96}
-                      src={instaIcon.src}
-                      alt="Instagram icon"
-                    />
-                  </div>
+                </figure>
+
+                <div className="absolute -top-4 -right-4">
+                  <Image
+                    className="w-12 rounded-full"
+                    width={96}
+                    height={96}
+                    src={instaIcon.src}
+                    alt="Instagram icon"
+                  />
                 </div>
-                <div className="relative left-1/3 -top-12">
-                  <div className="w-56 bg-ui-white shadow-lg relative">
-                    <Image
-                      width={224}
-                      height={400}
-                      src={facebookImage.src}
-                      alt="Facebook"
-                      className="rounded-xl"
-                    />
-                    <div className="absolute -top-4 -right-4">
-                      <Image
-                        className="w-10"
-                        width={96}
-                        height={96}
-                        src={facebookIcon.src}
-                        alt="Facebook icon"
-                      />
-                    </div>
-                  </div>
+              </div>
+
+              <div className="w-full ms-[80%] -mt-16 relative">
+                <figure className="w-full aspect-[5/6] rounded-3xl shadow-lg overflow-hidden">
+                  <Image
+                    width={224}
+                    height={400}
+                    src={facebookImage.src}
+                    alt="Facebook"
+                    className="w-full h-full rounded-3xl object-cover hover:scale-110 duration-1000"
+                  />
+                </figure>
+
+                <div className="absolute -top-4 -right-4">
+                  <Image
+                    className="w-12"
+                    width={96}
+                    height={96}
+                    src={facebookIcon.src}
+                    alt="Facebook icon"
+                  />
                 </div>
               </div>
             </div>
-            <div className="mt-6">
-              <div className="w-56 bg-ui-white shadow-lg relative">
+
+            <div className="relative">
+              <figure className="w-52 aspect-[3/5] rounded-3xl shadow-lg overflow-hidden">
                 <Image
                   width={224}
                   height={400}
                   src={tiktokImage.src}
                   alt="TikTok"
-                  className="rounded-xl h-96 object-cover"
+                  className="w-full h-full rounded-3xl object-cover hover:scale-110 duration-1000"
                 />
-                <div className="absolute -top-4 -right-4">
-                  <Image
-                    className="w-10"
-                    width={96}
-                    height={96}
-                    src={tiktokIcon.src}
-                    alt="TikTok icon"
-                  />
-                </div>
+              </figure>
+
+              <div className="absolute -top-4 -right-4">
+                <Image
+                  className="w-12"
+                  width={96}
+                  height={96}
+                  src={tiktokIcon.src}
+                  alt="TikTok icon"
+                />
               </div>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="lg:hidden">
+      <section className="sm:hidden">
         <h2 className="text-6xl font-bold">Find Us On Socials</h2>
       </section>
     </>
