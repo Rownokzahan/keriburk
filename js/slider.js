78a887c0a32f87ef2ceb1a88c1e8c9247dfb8f45
Rownok Zahan <rownokzahan9@gmail.com>
2024-10-22 16:25:42 +0600

    Add Hero Slider Section

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(function () {
+  $("#hero-slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    pauseOnHover: true,
+  });
+});
