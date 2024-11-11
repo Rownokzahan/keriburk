feaac36e8bc22bdd77a849aa3a17d217c4665afe
Rownok Zahan <rownokzahan9@gmail.com>
2024-11-11 17:24:44 +0600

    Update Offers

--- js/slider.js
+++ js/slider.js
@@ -10,6 +10,17 @@ $(function () {
     pauseOnHover: true,
   });
 
+  $("#offer-desktop-slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+  });
+
   $("#recipe-slider").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
