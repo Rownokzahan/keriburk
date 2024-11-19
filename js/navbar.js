0522b8cc704c0920e089262d31aef70aeb5c5938
Rownok Zahan <rownokzahan9@gmail.com>
2024-11-19 18:27:21 +0600

    Update Navbar Styles

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,9 @@
-// $(document).ready(function () {
-//   $(window).on("scroll", function () {
-//     if ($(window).scrollTop() > 100) {
-//       $("header").addClass("sticky-navbar");
-//       $("main").addClass("mt-[64px] md:mt-[66px]");
-//     } else {
-//       $("header").removeClass("sticky-navbar");
-//       $("main").removeClass("mt-[64px] md:mt-[66px]");
-//     }
-//   });
-// });
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 0) {
+      $("#navbar").addClass("animate-fadeInDown");
+    } else {
+      $("#navbar").removeClass("animate-fadeInDown");
+    }
+  });
+});
