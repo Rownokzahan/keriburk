881806ee6e35e083ce628dbfa71d800277dbca78
Rownok Zahan <rownokzahan9@gmail.com>
2024-12-05 17:40:48 +0600

    Organize and fix spelling mistakes

--- js/stores-data.js
+++ js/stores-data.js
@@ -6,147 +6,130 @@ const storesData = [
       {
         name: "Dhaka",
         stores: [
-          { id: "D001", name: "Postogola", storeType: "Super Store" },
-          { id: "D002", name: "Uttara-6", storeType: "Super Store" },
-          { id: "D003", name: "Uttara-11", storeType: "Mega Store" },
-          { id: "D004", name: "Malibagh", storeType: "Super Store" },
-          { id: "D005", name: "Green Road", storeType: "Super Store" },
-          { id: "D006", name: "Nazim Uddin Road", storeType: "Super Store" },
-          { id: "D007", name: "West Kafrul", storeType: "Super Store" },
-          { id: "D008", name: "Sutrapur", storeType: "Super Store" },
-          { id: "D009", name: "Khilgaon", storeType: "Super Store" },
-          { id: "D010", name: "Banani", storeType: "Super Store" },
-          { id: "D011", name: "Banani-2", storeType: "Super Store" },
-          { id: "D012", name: "Nikunja-2", storeType: "Super Store" },
-          { id: "D013", name: "Gulshan-1", storeType: "Super Store" },
           {
-            id: "D014",
-            name: "Gulshan-2 (New & Old)",
-            storeType: "Super Store",
-          },
-          { id: "D015", name: "Shahjahan Road", storeType: "Super Store" },
-          { id: "D016", name: "Azimpur", storeType: "Super Store" },
-          { id: "D017", name: "Khilkhet Lake City", storeType: "Super Store" },
-          { id: "D018", name: "North Banasree", storeType: "Super Store" },
-          { id: "D019", name: "Central Basabo-2", storeType: "Mega Store" },
-          { id: "D020", name: "Sontek Kajla", storeType: "Super Store" },
-          { id: "D021", name: "Mohanagar", storeType: "Super Store" },
-          { id: "D022", name: "Mirpur-1", storeType: "Super Store" },
-          { id: "D023", name: "Mirpur-2", storeType: "Super Store" },
-          { id: "D024", name: "Mirpur-6 (New)", storeType: "Super Store" },
-          { id: "D025", name: "Mirpur-10", storeType: "Super Store" },
-          { id: "D026", name: "Mirpur-11", storeType: "Super Store" },
-          { id: "D027", name: "Mirpur-12", storeType: "Super Store" },
-          { id: "D028", name: "Mirpur Borobagh", storeType: "Super Store" },
-          { id: "D029", name: "Mirpur-12 Bus Stand", storeType: "Super Store" },
-          { id: "D030", name: "Taltola Agargaon", storeType: "Super Store" },
-          { id: "D031", name: "Bashundhara Block-G", storeType: "Mega Store" },
-          { id: "D032", name: "Kamrangir Chor", storeType: "Super Store" },
-          {
-            id: "D033",
             name: "Aftab Nagar (Siraj Community Center)",
             storeType: "Super Store",
           },
-          {
-            id: "D034",
-            name: "Konapara Demra",
-            storeType: "Convenience Store",
-          },
-          { id: "D035", name: "Golapbagh", storeType: "Convenience Store" },
-          { id: "D036", name: "Saterkul", storeType: "Convenience Store" },
-          {
-            id: "D037",
-            name: "Hasnabad Outlet",
-            storeType: "Convenience Store",
-          },
-          {
-            id: "D038",
-            name: "Ashkona Outlet",
-            storeType: "Convenience Store",
-          },
-          {
-            id: "D039",
-            name: "Mohammadpur (Ring Road & Bosila)",
-            storeType: "Convenience Store",
-          },
-          { id: "D040", name: "Moddho Badda", storeType: "Convenience Store" },
-          { id: "D041", name: "Dhaka Housing", storeType: "Super Store" },
-          { id: "D042", name: "Kaderabad Housing", storeType: "Super Store" },
-          { id: "D043", name: "Dhanmondi-27", storeType: "Mega Store" },
-          { id: "D044", name: "Panthapath (New)", storeType: "Super Store" },
+          { name: "Ashkona Outlet", storeType: "Convenience Store" },
+          { name: "Azimpur", storeType: "Super Store" },
+          { name: "Banani", storeType: "Super Store" },
+          { name: "Banani-2", storeType: "Super Store" },
+          { name: "Bashundhara Block-G", storeType: "Mega Store" },
+          { name: "Central Basabo-2", storeType: "Mega Store" },
+          { name: "Dhaka Housing", storeType: "Super Store" },
+          { name: "Dhanmondi-27", storeType: "Mega Store" },
+          { name: "Golapbagh", storeType: "Convenience Store" },
+          { name: "Gopibagh", storeType: "Mega Store" },
+          { name: "Green Road", storeType: "Mega Store" },
+          { name: "Gulshan-1", storeType: "Mega Store" },
+          { name: "Gulshan-2 New", storeType: "Super Store" },
+          { name: "Hasnabad Outlet", storeType: "Convenience Store" },
+          { name: "Kaderabad Housing", storeType: "Super Store" },
+          { name: "Kamrangir Chor", storeType: "Super Store" },
+          { name: "Khilgaon", storeType: "Super Store" },
+          { name: "Khilkhet Lake City", storeType: "Super Store" },
+          { name: "Mirpur-1", storeType: "Mega Store" },
+          { name: "Mirpur-10, Block-C", storeType: "Super Store" },
+          { name: "Mirpur-11", storeType: "Super Store" },
+          { name: "Mirpur-12", storeType: "Super Store" },
+          { name: "Mirpur-12 Bus Stand", storeType: "Super Store" },
+          { name: "Mirpur 14", storeType: "Super Store" },
+          { name: "Mirpur Borobagh", storeType: "Convenience Store" },
+          { name: "Mirpur-2", storeType: "Super Store" },
+          { name: "Mirpur-6 (New)", storeType: "Super Store" },
+          { name: "Mohanagar", storeType: "Super Store" },
+          { name: "Moddho Badda", storeType: "Super Store" },
+          { name: "Monipuri Para Outlet", storeType: "Super Store" },
+          { name: "Mugdha Outlet", storeType: "Convenience Store" },
+          { name: "Nakhal Para", storeType: "Convenience Store" },
+          { name: "Nikunja-2", storeType: "Super Store" },
+          { name: "North Banasree", storeType: "Super Store" },
+          { name: "Notun Bazar", storeType: "Super Store" },
+          { name: "Panthapath (New)", storeType: "Super Store" },
+          { name: "Postogola", storeType: "Super Store" },
+          { name: "Ring Road Mohammadpur", storeType: "Super Store" },
+          { name: "Savar Cantonment", storeType: "Super Store" },
+          { name: "Savar New Market", storeType: "Super Store" },
+          { name: "Savar Thana Road", storeType: "Super Store" },
+          { name: "Shahjahan Road", storeType: "Super Store" },
+          { name: "Sonir Akhra outlet", storeType: "Convenience Store" },
+          { name: "Sontek Kajla", storeType: "Super Store" },
+          { name: "South Keraniganj Hasnabad", storeType: "Super Store" },
+          { name: "Sutrapur", storeType: "Super Store" },
+          { name: "Taltola Agargaon", storeType: "Super Store" },
+          { name: "Uttara-11", storeType: "Mega Store" },
+          { name: "Uttara-3", storeType: "Mega Store" },
+          { name: "Uttara 3-Robindro Sarani", storeType: "Mega Store" },
+          { name: "Uttara-6", storeType: "Super Store" },
+          { name: "Wari New", storeType: "Mega Store" },
+          { name: "West Kafrul", storeType: "Super Store" },
+          { name: "Jatrabari-2", storeType: "Convenience Store" },
+          { name: "Jigatola Outlet", storeType: "Convenience Store" },
+          { name: "Konapara Demra", storeType: "Convenience Store" },
+          { name: "Merul Badda", storeType: "Convenience Store" },
+          { name: "Matikata Outlet", storeType: "Super Store" },
+          { name: "New Eskaton", storeType: "Super Store" },
+          { name: "Saterkul", storeType: "Convenience Store" },
         ],
       },
       {
         name: "Gazipur",
         stores: [
+          { name: "Joydebpur Outlet", storeType: "Convenience Store" },
           {
-            id: "G001",
-            name: "Joydebpur Outlet",
+            name: "Tongi College Road Express",
             storeType: "Convenience Store",
           },
-          { id: "G002", name: "Tongi Outlet", storeType: "Super Store" },
-          {
-            id: "G003",
-            name: "Tongi College Road",
-            storeType: "Convenience Store",
-          },
+          { name: "Tongi Outlet", storeType: "Convenience Store" },
         ],
       },
       {
         name: "Manikganj",
-        stores: [
-          { id: "M001", name: "Manikganj Bus Stand", storeType: "Super Store" },
-        ],
+        stores: [{ name: "Manikganj Bus Stand", storeType: "Super Store" }],
       },
       {
         name: "Narayanganj",
         stores: [
-          { id: "N001", name: "Masdair Narayanganj", storeType: "Super Store" },
-          { id: "N002", name: "Narayanganj Outlet", storeType: "Super Store" },
+          { name: "Masdair Narayanganj", storeType: "Super Store" },
+          { name: "Narayangong", storeType: "Convenience Store" },
+          { name: "Narayanganj Outlet", storeType: "Super Store" },
         ],
       },
       {
-        name: "Savar",
+        name: "Brahmanbaria",
         stores: [
-          { id: "S001", name: "Savar Cantonment", storeType: "Super Store" },
-          { id: "S002", name: "Savar Thana Road", storeType: "Super Store" },
-          { id: "S003", name: "Savar New Market", storeType: "Super Store" },
+          {
+            name: "Brahmanbaria Outlet",
+            storeType: "Convenience Store",
+          },
+          { name: "Brahmanbaria Sadar", storeType: "Super Store" },
         ],
       },
     ],
   },
   {
     id: "2",
-    division: "Chattogram",
+    division: "Chittagong",
     districts: [
       {
-        name: "Chattogram",
+        name: "Chittagong",
         stores: [
-          { id: "C001", name: "Gol Pahar", storeType: "Super Store" },
-          { id: "C002", name: "Hali Shohor", storeType: "Super Store" },
-          { id: "C003", name: "Khulshi", storeType: "Super Store" },
-          { id: "C004", name: "Jamal Khan", storeType: "Super Store" },
-          {
-            id: "C005",
-            name: "Kolatoli (Cox's Bazar)",
-            storeType: "Super Store",
-          },
-          {
-            id: "C006",
-            name: "Jhautola (Cox's Bazar)",
-            storeType: "Super Store",
-          },
+          { name: "Gol Pahar", storeType: "Super Store" },
+          { name: "Hali Shohor", storeType: "Super Store" },
+          { name: "Jamal Khan", storeType: "Super Store" },
+          { name: "Jhautola (Cox's Bazar)", storeType: "Super Store" },
+          { name: "Khulshi", storeType: "Mega Store" },
+          { name: "Kolatoli (Cox's Bazar)", storeType: "Super Store" },
         ],
       },
       {
         name: "Feni",
-        stores: [{ id: "F001", name: "Feni Outlet", storeType: "Super Store" }],
+        stores: [{ name: "Feni Outlet", storeType: "Convenience Store" }],
       },
       {
         name: "Noakhali",
         stores: [
           {
-            id: "N001",
             name: "Maijdee Outlet",
             storeType: "Convenience Store",
           },
@@ -155,11 +138,20 @@ const storesData = [
       {
         name: "Comilla",
         stores: [
-          { id: "C001", name: "Comilla Outlet", storeType: "Super Store" },
-          { id: "C002", name: "Comilla-2 Outlet", storeType: "Super Store" },
           {
-            id: "C003",
             name: "Chandina Outlet",
+            storeType: "Convenience Store",
+          },
+          { name: "Comilla Outlet", storeType: "Super Store" },
+          { name: "Comilla-2 Outlet", storeType: "Super Store" },
+        ],
+      },
+      {
+        name: "Chandpur",
+        stores: [
+          { name: "Hajigonj Outlet", storeType: "Super Store" },
+          {
+            name: "Mission Road Outlet",
             storeType: "Convenience Store",
           },
         ],
@@ -173,16 +165,15 @@ const storesData = [
       {
         name: "Khulna",
         stores: [
-          { id: "K001", name: "Boyra", storeType: "Super Store" },
-          { id: "K002", name: "South Central Road", storeType: "Super Store" },
-          { id: "K003", name: "Sonadanga", storeType: "Super Store" },
+          { name: "Boyra", storeType: "Super Store" },
+          { name: "South Central Road", storeType: "Super Store" },
+          { name: "Sonadanga", storeType: "Super Store" },
         ],
       },
       {
         name: "Kushtia",
         stores: [
           {
-            id: "K004",
             name: "Shapla Chattar Outlet",
             storeType: "Convenience Store",
           },
@@ -197,22 +188,17 @@ const storesData = [
       {
         name: "Rajshahi",
         stores: [
-          { id: "R001", name: "Alupatti", storeType: "Super Store" },
-          { id: "R002", name: "Nagar Bhaban Outlet", storeType: "Super Store" },
-          {
-            id: "R003",
-            name: "Naogaon Outlet",
-            storeType: "Convenience Store",
-          },
+          { name: "Alupatti", storeType: "Super Store" },
+          { name: "Naogaon Outlet", storeType: "Super Store" },
+          { name: "Nagar Bhaban Outlet", storeType: "Convenience Store" },
         ],
       },
       {
-        name: "Bagura",
+        name: "Bogura",
         stores: [
           {
-            id: "B001",
             name: "Malati Nagar Outlet",
-            storeType: "Convenience Store",
+            storeType: "Super Store",
           },
         ],
       },
@@ -224,7 +210,7 @@ const storesData = [
     districts: [
       {
         name: "Barishal",
-        stores: [{ id: "B001", name: "Sadar Road", storeType: "Super Store" }],
+        stores: [{ name: "Sadar Road", storeType: "Super Store" }],
       },
     ],
   },
@@ -235,27 +221,18 @@ const storesData = [
       {
         name: "Sylhet",
         stores: [
-          { id: "S001", name: "Shahjalal Outlet", storeType: "Super Store" },
-          { id: "S002", name: "Shibgong Outlet", storeType: "Super Store" },
-          {
-            id: "S003",
-            name: "Pathantola Outlet",
-            storeType: "Convenience Store",
-          },
-          { id: "S004", name: "Zinda Bazar", storeType: "Super Store" },
-          { id: "S005", name: "Amberkhana Outlet", storeType: "Super Store" },
-          {
-            id: "S006",
-            name: "Beanibazar Outlet",
-            storeType: "Convenience Store",
-          },
+          { name: "Amberkhana Outlet", storeType: "Super Store" },
+          { name: "Beanibazar Outlet", storeType: "Super Store" },
+          { name: "Pathantola Outlet", storeType: "Convenience Store" },
+          { name: "Shahjalal Outlet", storeType: "Super Store" },
+          { name: "Shibgong Outlet", storeType: "Super Store" },
+          { name: "Zinda Bazar", storeType: "Mega Store" },
         ],
       },
       {
         name: "Moulvibazar",
         stores: [
           {
-            id: "M001",
             name: "Moulvibazar Outlet",
             storeType: "Convenience Store",
           },
@@ -265,7 +242,6 @@ const storesData = [
         name: "Habiganj",
         stores: [
           {
-            id: "H001",
             name: "Habiganj Outlet",
             storeType: "Convenience Store",
           },
@@ -279,9 +255,7 @@ const storesData = [
     districts: [
       {
         name: "Rangpur",
-        stores: [
-          { id: "R001", name: "Shyamoli Outlet", storeType: "Super Store" },
-        ],
+        stores: [{ name: "Shyamoli Outlet", storeType: "Super Store" }],
       },
     ],
   },
@@ -319,4 +293,15 @@ export function getStoresByDistrict(district) {
   }
 
   return [];
+}
+
+export function getAllDistricts() {
+  const allDistricts = storesData.reduce((districts, division) => {
+    const divisionDistricts = division.districts.map(
+      (district) => district.name
+    );
+    return districts.concat(divisionDistricts);
+  }, []);
+
+  return allDistricts;
 }
